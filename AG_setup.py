2d4bce79b509f5be5c19da8c3cc6c72c01b32cb8
SzymonTokarzProgramista <149623510+SzymonTokarzProgramista@users.noreply.github.com>
2023-12-15 18:59:26 +0100

    debug

--- AG_setup.py
+++ AG_setup.py
@@ -4,7 +4,7 @@ import numpy as np
 def rows_columns(): #losowe wybranie ilości uczniów, liczby przedmiotów i limitów w grupach
     rows=random.randint(50,120)
     columns=random.randint(4,8)
-    limits = [random.randint(40, 80) for _ in range(columns)]
+    limits = [random.randint(40, 80) for subject in range(columns)]
     return rows,columns,limits
 
 
@@ -17,14 +17,11 @@ def create_matrix(rows,columns):
 
 
 def create_mask(rows,columns): #tworzenie maski z 0 i 1
-    mask=[]
-    for i in range(rows):
-        for j in range(columns):
-            mask[i][j]=random.randint(0,1)
+    mask = np.random.randint(2, size=(rows, columns))
     return mask
 
 
-def starting_parents(row,columns):
+def starting_parents(rows,columns):
     p1=create_matrix(rows,columns)
     p2=create_matrix(rows,columns)
     return p1,p2
--- alogrithm.py
+++ alogrithm.py
@@ -1,18 +1,17 @@
 import AG_setup as ag
+import numpy as np
 
 
 def crossing(): #wstępne krzyżowanie
     r_c=ag.rows_columns()
     rows=r_c[0]
     columns=r_c[1]
-    limits=r_c[2]
-    original=ag.create_matrix(rows,columns)
     mask=ag.create_mask(rows,columns)
-    parents=ag.starting_parents()
+    parents=ag.starting_parents(rows,columns)
     p1=parents[0]
     p2=parents[1]
-    child1=np.empty(rows,columns)
-    child2=np.empty(rows,columns)
+    child1=np.empty(shape=(rows,columns))
+    child2=np.empty(shape=(rows,columns))
     for i in range(rows):
         for j in range(columns):
             if mask[i][j]==0 and p1[i][j]>p2[i][j]: #jeżeli maska to 0 to bierze tego z mniejszą wartością
@@ -25,7 +24,9 @@ def crossing(): #wstępne krzyżowanie
 
 children=crossing()
 child1=children[0]
+child2=children[1]
 print(child1)
+print(child2)
 
 
 
