d915b64411ddca66513c80ada420209a9c5e609e
SzymonTokarzProgramista <149623510+SzymonTokarzProgramista@users.noreply.github.com>
2023-12-15 18:14:09 +0100

    xd

--- alogrithm.py
+++ alogrithm.py
@@ -11,13 +11,19 @@ def crossing():
     parents=starting_parents()
     p1=parents[0]
     p2=parents[1]
-    child=np.empty(rows,columns)
+    child1=np.empty(rows,columns)
+    child2=np.empty(rows,columns)
     for i in range(rows):
         for j in range(columns):
             if mask[i][j]==0 and p1[i][j]>p2[i][j]:
-                child[i][j]=p2[i][j]
+                child1[i][j]=p2[i][j]
+                child2[i][j] = p1[i][j]
             else:
-                child[i][j] = p1[i][j]
+                child1[i][j] = p1[i][j]
+                child2[i][j] = p1[i][j]
+    return child1,child2
+
+
 
 
 
