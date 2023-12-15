b42624c6e7945f4c1bd36fbd29676709ad44ce9d
SzymonTokarzProgramista <149623510+SzymonTokarzProgramista@users.noreply.github.com>
2023-12-15 18:11:56 +0100

    crossing pewnie chuja działa

--- AG_setup.py
+++ AG_setup.py
@@ -1,7 +1,7 @@
 import random
 import numpy
 
-def rows_columns():
+def rows_columns(): #losowe wybranie ilości uczniów, liczby przedmiotów i limitów w grupach
     rows=random.randint(50,120)
     columns=random.randint(4,8)
     for i in range(0,columns):
@@ -12,11 +12,11 @@ def rows_columns():
 def create_matrix(rows,columns):
     matrix=np.empty(rows,columns)
     for i in range(rows):
-        matrix[i]=random.sample(range(1,columns),columns)
+        matrix[i]=random.sample(range(1,columns),columns) #wypisywanie tabeli z preferencjammi od 1 do liczby przedmiotów
     return matrix
 
 
-def create_mask(rows,columns):
+def create_mask(rows,columns): #tworzenie maski z 0 i 1
     if type(rows) or type(columns) != int:
         return NULL
     else:
@@ -25,6 +25,12 @@ def create_mask(rows,columns):
             for j in range(columns):
                 mask[i][j]=random.randint(0,1)
     return mask
+
+
+def starting_parents(row,columns):
+    p1=create_matrix(rows,columns)
+    p2=create_matrix(rows,columns)
+    return p1,p2
 
 
 
--- alogrithm.py
+++ alogrithm.py
@@ -1,6 +1,23 @@
 import AG_setup.py
 
 
-def mutation(k):
-    
+def crossing():
+    r_c=rows_columns()
+    rows=r_c[0]
+    columns=r_c[1]
+    limits=r_c[2]
+    original=create_matrix(rows,columns)
+    mask=create_mask(rows,columns)
+    parents=starting_parents()
+    p1=parents[0]
+    p2=parents[1]
+    child=np.empty(rows,columns)
+    for i in range(rows):
+        for j in range(columns):
+            if mask[i][j]==0 and p1[i][j]>p2[i][j]:
+                child[i][j]=p2[i][j]
+            else:
+                child[i][j] = p1[i][j]
+
+
 
