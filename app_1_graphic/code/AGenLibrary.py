b7d274e980439fc5669b3a1637e4408d39782686
Virtual32PRO <152421459+Virtual32PRO@users.noreply.github.com>
2023-12-18 20:23:02 +0100

    App1 wersja graficzna

---
+++ app_1_graphic/code/AGenLibrary.py
@@ -0,0 +1,67 @@
+from abc import abstractmethod, ABC
+from random import random, choice
+is_running = False
+list_x=[]
+list_y=[]
+
+class Element(ABC):
+
+    def __init__(self):
+        self.fitness = self.evaluate_function()
+
+    def mutation(self):
+        self._perform_mutation()
+        self.fitness = self.evaluate_function()
+
+    @abstractmethod
+    def _perform_mutation(self):
+        pass
+
+    @abstractmethod
+    def crossover(self, element2: 'Element' ) -> 'Element':
+        pass
+
+    @abstractmethod
+    def evaluate_function(self):
+        pass
+
+class GeneticAlgorithm:
+
+    def __init__(self, first_population_generator: callable,
+                 selection_model: callable, stop_condition: callable, mutation_probability: float = 0.1):
+        self.first_generation_func = first_population_generator
+        self.selection_model = selection_model
+        self.stop_condition = stop_condition
+        self.mutation_probability = mutation_probability
+
+    def run(self):
+        population = self.first_generation_func()
+        population.sort(key=lambda x: -x.fitness)
+        population_len = len(population)
+        i = 0
+        global list_x
+        list_x=[]
+        global list_y
+        list_y=[]
+        global is_running
+        while True and is_running:
+            is_running = True
+            selected = self.selection_model(population)
+            new_population = selected.copy()
+            while len(new_population) != population_len:
+                child = choice(population).crossover(choice(population))
+                if random() <= self.mutation_probability:
+                    child.mutation()
+                new_population.append(child)
+
+            population = new_population
+            the_best_match = max(population, key=lambda x: x.fitness)
+            list_x.append(i)
+            list_y.append(the_best_match.fitness)
+            print("Iteration number: {} Matrix: {} fitness: {}".format(i, the_best_match, the_best_match.fitness))
+            i += 1
+            if self.stop_condition(i):
+                break
+
+
+
---
+++ app_1_graphic/code/main.py
@@ -0,0 +1,145 @@
+import tkinter as tk
+import threading
+from AGenLibrary import Element, GeneticAlgorithm
+from random import sample, randint
+import numpy as np
+import AGenLibrary
+from frontend import PlottingApp, LabelManager,ButtonManager,EntryManager
+import frontend
+
+#Parameters
+p = 100  # population size
+pc = 0.2  # elite percentage
+pm = 0.01  # mutations
+ps = 0.05  # mutation severity
+
+
+
+#general codes section
+
+def create_matrix_in(n,m):
+    matrix = [0 for _ in range(m) for _ in range(n)]
+    for i in range(n):
+        matrix[i]=sample(range(1,m+1),m)
+    return matrix
+
+def elite_selection_model(generation):
+    max_selected = int(len(generation) * pc)
+    sorted_by_assess = sorted(generation, key=lambda x: -x.fitness)
+    return sorted_by_assess[:max_selected]
+
+def stop_condition(iteration):
+    return iteration==frontend.MAX_ITERATION
+
+def first_population_generator():
+    return [ScheduleMatrix(create_matrix(frontend.n,frontend.m)) for _ in range(p)]
+
+def create_matrix(n, m):
+    matrix = []
+    for _ in range(n):
+        verse=random_verse(m)
+        matrix.append(verse)
+    return matrix
+def random_verse(m):
+    verse = [0 for _ in range(m)]
+    address = sample(range(m), 3)
+    for i in address:
+        verse[i] = 1
+    return verse
+
+def create_mask(n):
+    return [randint(0,1) for _ in range(n)]
+
+def update_variables(entry_manager):
+    global p, pc, pm, ps
+
+    values = entry_manager.get_values()
+
+    try:
+        if values['p']:
+            p = int(values['p'])
+        if values['pc']:
+            pc = float(values['pc'])
+        if values['pm']:
+            pm = float(values['pm'])
+        if values['ps']:
+            ps = float(values['ps'])
+        if values['iter']:
+            frontend.MAX_ITERATION = int(values['iter'])
+        if values['n']:
+            frontend.n = int(values['n'])
+        if values['m']:
+            frontend.m = int(values['m'])
+    except ValueError as e:
+        print("Wprowadzono nieprawidłowe dane:", e)
+def start():
+
+    AGenLibrary.is_running = True
+    threading.Thread(target=genetic_algorithm.run).start()
+
+def stop():
+
+    AGenLibrary.is_running = False
+
+class ScheduleMatrix(Element):
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.n=len(matrix)
+        self.m=len(matrix[0])
+        super().__init__()
+
+    def _perform_mutation(self):
+        for _ in range(int(ps*frontend.n)):
+            random_index = randint(0, n-1)
+            self.matrix[random_index]=random_verse(m)
+
+
+    def crossover(self, element2: 'Element' ) -> 'Element':
+        mask=create_mask(self.n)
+        child=[]
+        for i in range(self.n):
+            decision=mask[i]
+            if decision ==1:
+                child.append(self.matrix[i])
+            else:
+                child.append(element2.matrix[i])
+        return ScheduleMatrix(child)
+
+
+    def evaluate_function(self):
+        matrix1 = np.array(self.matrix)
+        matrix2 = np.array(frontend.MATRIX_IN)
+        product = np.multiply(matrix1, matrix2)
+        return np.sum(product)
+
+
+    def __repr__(self):
+        return str(self.matrix)
+
+
+if __name__ == "__main__":
+    genetic_algorithm = GeneticAlgorithm(
+        first_population_generator=first_population_generator,
+        selection_model=elite_selection_model,
+        stop_condition=stop_condition,
+        mutation_probability=pm
+    )
+    window = tk.Tk()
+    window.title("Evolutionary Algorithm")
+    window.geometry("1300x700")
+
+    app = PlottingApp(window)
+
+    label_manager = LabelManager(window,frontend.MATRIX_IN)
+    label_manager.place_labels()
+
+    entry_manager = EntryManager(window)
+    values = entry_manager.get_values()
+    update_variables(entry_manager)
+
+    button_manager = ButtonManager(window, start, stop, update_variables,entry_manager,label_manager)
+    button_manager.place_buttons()
+
+    window.mainloop()
+
